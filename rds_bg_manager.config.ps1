0bb6f90676d81aecd33661b82b978d41e441b6f1
wojtulab <wojtulab@gmail.com>
2026-02-12 11:52:17 +0100

    Update UpdateUrl to point to new repository

--- rds_bg_manager.config.ps1
+++ rds_bg_manager.config.ps1
@@ -1,5 +1,5 @@
 @{
     WindowTitle = "AWS RDS BLUE/GREEN DEPLOYMENT TOOL by WK"
-    UpdateUrl = "https://raw.githubusercontent.com/wojtulab/bg-deploy/refs/heads/update-monitor-snapshot-items-10088057860666066331/rds_bg_manager.ps1"
+    UpdateUrl = "https://raw.githubusercontent.com/wojtulab/blue-green-aws/refs/heads/main/rds_bg_manager.ps1"
     LogFileFormat = "rds_manager_{0:yyyyMMdd}.log"
 }
